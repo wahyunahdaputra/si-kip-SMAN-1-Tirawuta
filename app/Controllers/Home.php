e089ea7de7a76fc54f03becf6b5e2f0e9c5b3b50
wahyunahdaputra <nndaptrawahyu@gmail.com>
2024-02-08 10:13:47 +0800

    Update: template, paginasi, program stud -> home

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,12 +2,36 @@
 
 namespace App\Controllers;
 
+use App\Models\HomeModel;
+
 
 class Home extends BaseController
 {
-    public function index(): string
-    {
-        return view('data/home');
+    protected $mhome;
+    protected $mpagination;
+
+    public function __construct(){
+
+        $this->mhome = new HomeModel();
     }
 
+    public function index()
+    {
+        $getdata = $this->mhome->getdata();
+
+    //    $filter_params = array(
+    //     'fakultas' => $this -> input -> get('fakultas'),
+    //     'program'  => $this -> input -> get('fakultas'),
+    //    );
+    //    $get_filtered_data = $this -> mprogram_studi -> getFilteredData();
+
+       $data = array(
+            'dataProgramStudi' => $getdata,
+            'paginasi' => $this->mhome->paginate(30, 'paginasi'), // Mengambil data yang dipaginasi
+            'pager' => $this->mhome->pager
+       );
+
+    //    var_dump($getdata);
+        echo view('data/home', $data);
+    }
 }
--- app/Models/HomeModel.php
+++ app/Models/HomeModel.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class ProgramStudiModel extends Model
+class HomeModel extends Model
 {
+    protected $table = 'program_studi';
+    protected $allowedFields = ['fakultas', 'program', 'program_studi', 'pr_akreditasi', 'tgl_sk', 'tgl_kadaluarsa', 'no_sk', 'tgl_hariini', 'status_kadaluarsa', 'akr_oleh'];
+
     public function getdata()
     {
         $query = $this->db->query("SELECT * FROM program_studi");
